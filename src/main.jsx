523f6c958129d04401325cd9669f0e5ad34c9c41
ayushcode12 <ayushjain8477@gmail.com>
2025-12-08 22:28:46 +0530

    Configure axios for Render backend

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,20 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter } from 'react-router-dom';
 import { StoreContextProvider } from './context/StoreContext.jsx';
+import axios from 'axios';
+
+axios.interceptors.request.use((config) => {
+  if (config.url.startsWith("http://localhost:8080")) {
+    config.url = config.url.replace(
+      "http://localhost:8080",
+      "https://foodies-backend-e6v6.onrender.com"
+    );
+  }
+  return config;
+});
+
+axios.defaults.baseURL = "https://foodies-backend-e6v6.onrender.com";
+axios.defaults.headers.common["Content-Type"] = "application/json";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
